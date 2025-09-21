3a40e55bc5647085a9ea4674a3a1ccab67a2fdc5
GhadaAdel <gh.adel.aa@gmail.com>
2025-09-21 16:28:20 +0300

    feat: adding forgot and reset password logic APIs

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,10 +2,15 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\LoginRequest;
-use App\Http\Requests\RegisterRequest;
-use App\Http\Resources\UserResource;
+use App\Models\User;
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
+use App\Http\Requests\LoginRequest;
+use App\Http\Requests\ForgotPasswordRequest;
+use App\Http\Requests\ResetPasswordRequest;
+use App\Http\Resources\UserResource;
+use App\Http\Requests\RegisterRequest;
+use Illuminate\Support\Facades\Password;
 
 class AuthController extends Controller
 {
@@ -46,5 +51,36 @@ class AuthController extends Controller
         return response([
             'message' => 'You are logged out successfully!'
         ]);
+    }
+
+    public function forgotPassword(ForgotPasswordRequest $request)
+    {
+        $status = $request->sendResetLink();
+
+        if ($status === Password::RESET_LINK_SENT) {
+            return response([
+                'message' => __($status)
+            ], 200);
+        }
+
+        return response([
+            'message' => __($status)
+        ], 400);
+    }
+
+    
+    public function resetPassword(ResetPasswordRequest $request)
+    {
+        $status = $request->resetPassword();
+
+        if ($status === Password::PASSWORD_RESET) {
+            return response([
+                'message' => __($status)
+            ], 200);
+        }
+
+        return response([
+            'message' => __($status)
+        ], 400);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -29,3 +29,5 @@ Route::middleware('auth:sanctum')->group(function () {
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
+Route::post('/forgot-password', [AuthController::class, 'forgotPassword']);
+Route::post('/reset-password', [AuthController::class, 'resetPassword']);
